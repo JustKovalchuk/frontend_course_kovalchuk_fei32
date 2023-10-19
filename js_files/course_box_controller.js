e3ca897616cc19dc76a50b2ea1831fafe8e93edb
Kovalchuk <sashakovalchuk1955@gmail.com>
2023-10-19 14:44:49 +0300

    CSS part1

---
+++ js_files/course_box_controller.js
@@ -0,0 +1,4 @@
+$(function(){
+    $('head').append('<link rel="stylesheet" href="/css_files/course-box.css" type="text/css" />');
+    $(".course-box-container").load("/elements/basic_elements/course-box.html");
+});
---
+++ js_files/header_footer_controller.js
@@ -0,0 +1,4 @@
+$(function(){
+    $("#header").load("/elements/basic_elements/header.html"); 
+    $("#footer").load("/elements/basic_elements/footer.html"); 
+});
